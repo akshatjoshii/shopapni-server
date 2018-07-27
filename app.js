949a98c59746daa688e0b6d0e73db28afeac3e89
Akshat Joshi <akshat2@gmail.com>
2018-07-27 22:39:14 +0530

    port added

--- app.js
+++ app.js
@@ -17,6 +17,6 @@ app.get('/', (req, res)=>{
    res.send("Hello World!");
 });
 
-app.listen(process.env.PORT, ()=>{
+app.listen(process.env.PORT ||3000, ()=>{
     console.log(`Server is running at ${process.env.PORT ||3000}`);
 });
