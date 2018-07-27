2d36d291dd5eb4f8f2098fe3a23d1222c3be82fb
Akshat Joshi <akshat2@gmail.com>
2018-07-27 21:30:48 +0530

    get api

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
 
+
+app.get('/', (req, res)=>{
+   res.send("Hello World!");
+});
+
 app.listen(process.env.PORT, ()=>{
     console.log(`Server is running at ${process.env.PORT ||3000}`);
 });
