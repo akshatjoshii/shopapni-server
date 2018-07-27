1e509d0b25dda5b608135e56a9e67e6cdf0593ba
Akshat Joshi <akshat2@gmail.com>
2018-07-27 19:58:52 +0530

    Initialization of Shopapni server

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan("dev"));
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server is running at ${process.env.PORT}`);
+});
